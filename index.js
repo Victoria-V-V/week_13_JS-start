a87b18891a71f56c2dca7a972759cb1b2a5ac2d8
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-01-14 19:42:03 +0700

    Added calculator

--- index.js
+++ index.js
@@ -2,3 +2,31 @@ function showWelcomeMessage(){
     let name1 = prompt('Как тебя зовут?');
     alert(`Привет, ${name1}!`);
 }
+
+function add(){
+    let number1 = prompt('Выберите первое число');
+    let number2 = prompt('Выберите второе число');
+    let result = (+number1 + +number2);
+    alert(`Результат сложения ${result}`);
+}
+
+function subtract(){
+    let number1 = prompt('Выберите первое число');
+    let number2 = prompt('Выберите второе число');
+    let result = (number1 - number2);
+    alert(`Результат вычитания ${result}`);
+}
+
+function divide(){
+    let number1 = prompt('Выберите первое число');
+    let number2 = prompt('Выберите второе число');
+    let result = (number1 / number2);
+    alert(`Результат деления ${result}`);
+}
+
+function multiplay(){
+    let number1 = prompt('Выберите первое число');
+    let number2 = prompt('Выберите второе число');
+    let result = (number1 * number2);
+    alert(`Результат умножения ${result}`);
+}
